fe23d8be4b810af5b2e4487bc1f4fb1c723dc892
Bart van Maris <Bartvanmaris@hotmail.nl>
2020-04-08 15:15:54 +0200

    reformated TurfViewController

--- src/main/main/contollers/TurfViewController.java
+++ src/main/main/contollers/TurfViewController.java
@@ -158,6 +158,27 @@ public class TurfViewController implements Initializable {
         beerDrankButtonList.add(beerDrank3);
         beerDrankButtonList.add(beerDrank4);
         beerDrankButtonList.add(beerDrank5);
+
+        misBeerButtonList.add(misBeer0);
+        misBeerButtonList.add(misBeer1);
+        misBeerButtonList.add(misBeer2);
+        misBeerButtonList.add(misBeer3);
+        misBeerButtonList.add(misBeer4);
+        misBeerButtonList.add(misBeer5);
+
+        kratKoopButtonList.add(kratKoop0);
+        kratKoopButtonList.add(kratKoop1);
+        kratKoopButtonList.add(kratKoop2);
+        kratKoopButtonList.add(kratKoop3);
+        kratKoopButtonList.add(kratKoop4);
+        kratKoopButtonList.add(kratKoop5);
+
+        misKratButtonList.add(misKrat0);
+        misKratButtonList.add(misKrat1);
+        misKratButtonList.add(misKrat2);
+        misKratButtonList.add(misKrat3);
+        misKratButtonList.add(misKrat4);
+        misKratButtonList.add(misKrat5);
     }
 
     public void setAccountList(AccountList accountList){
@@ -171,24 +192,14 @@ public class TurfViewController implements Initializable {
      * A method setting all the names in the view
      */
     public void setNames() {
-        naamAcc0.setText(accountList.get(0).getName());
-        naamAcc1.setText(accountList.get(1).getName());
-        naamAcc2.setText(accountList.get(2).getName());
-        naamAcc3.setText(accountList.get(3).getName());
-        naamAcc4.setText(accountList.get(4).getName());
-        naamAcc5.setText(accountList.get(5).getName());
+        for(int i = 0; i<6; i++) accountNameList.get(i).setText(accountList.get(i).getName());
     }
 
     /**
      * a method setting all the Stocks in the view
      */
     public void setAllStocks(){
-        stockAcc0.setText(String.valueOf(accountList.get(0).getStock()));
-        stockAcc1.setText(String.valueOf(accountList.get(1).getStock()));
-        stockAcc2.setText(String.valueOf(accountList.get(2).getStock()));
-        stockAcc3.setText(String.valueOf(accountList.get(3).getStock()));
-        stockAcc4.setText(String.valueOf(accountList.get(4).getStock()));
-        stockAcc5.setText(String.valueOf(accountList.get(5).getStock()));
+        for(int i = 0; i<6;i++) accountStockList.get(i).setText(String.valueOf(accountList.get(i).getStock()));
         totalStock.setText(String.valueOf(accountList.getTotalStock()));
     }
 
@@ -198,52 +209,17 @@ public class TurfViewController implements Initializable {
      */
     public void drankButtonClicked(ActionEvent event) {
         mainController.resetMessageBoard();
-        if(event.getSource() instanceof Button) {
-            Button eventButton = (Button) event.getSource();
-
-            //Updating the correct account
-            if(eventButton.equals(beerDrank0)){
-                accountList.beerDrank(0);
-                stockAcc0.setText(String.valueOf(accountList.get(0).getStock()));
-                if(accountList.get(0).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
-                }
-            } else if(eventButton.equals(beerDrank1)){
-                accountList.beerDrank(1);
-                stockAcc1.setText(String.valueOf(accountList.get(1).getStock()));
-                if(accountList.get(1).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
-                }
-            } else if(eventButton.equals(beerDrank2)){
-                accountList.beerDrank(2);
-                stockAcc2.setText(String.valueOf(accountList.get(2).getStock()));
-                if(accountList.get(2).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
-                }
-            } else if(eventButton.equals(beerDrank3)){
-                accountList.beerDrank(3);
-                stockAcc3.setText(String.valueOf(accountList.get(3).getStock()));
-                if(accountList.get(3).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
-                }
-            } else if(eventButton.equals(beerDrank4)){
-                accountList.beerDrank(4);
-                stockAcc4.setText(String.valueOf(accountList.get(4).getStock()));
-                if(accountList.get(4).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
-                }
-            } else if(eventButton.equals(beerDrank5)){
-                accountList.beerDrank(5);
-                stockAcc5.setText(String.valueOf(accountList.get(5).getStock()));
-                if(accountList.get(5).getStock()<0){
-                    mainController.getMessageBoard().setText("Ga bier kopen feut");
-                }
+        for(int i = 0; i<6 ; i++){
+            if(event.getSource().equals(beerDrankButtonList.get(i))){
+                accountList.beerDrank(i);
+                if(accountList.get(i).getStock()<0)  mainController.getMessageBoard().setText("Ga bier kopen met je hoofd");
+                accountStockList.get(i).setText(String.valueOf(accountList.get(i).getStock()));
             }
-            if(mainController.randomise((int)levelSlider.getValue())) mainController.getMessageBoard().setText("Trek een Spies Amice");
-            totalStock.setText(String.valueOf(accountList.getTotalStock()));
-            mainController.write();
-            positiveBeer();
         }
+        if(mainController.randomise((int)levelSlider.getValue())) mainController.getMessageBoard().setText("Trek een Spies Amice");
+        totalStock.setText(String.valueOf(accountList.getTotalStock()));
+        mainController.write();
+        positiveBeer();
     }
     /**
      * Method for handeling the misbeer button
@@ -251,33 +227,17 @@ public class TurfViewController implements Initializable {
      */
     public void misBeerButtonClicked(ActionEvent event){
         mainController.resetMessageBoard();
-        if(event.getSource() instanceof Button) {
-            Button eventButton = (Button) event.getSource();
-
-            if(eventButton.equals(misBeer0)) {
-                accountList.misBeer(0);
-                stockAcc0.setText(String.valueOf(accountList.get(0).getStock()));
-            } else if(eventButton.equals(misBeer1)) {
-                accountList.misBeer(1);
-                stockAcc1.setText(String.valueOf(accountList.get(1).getStock()));
-            } else if(eventButton.equals(misBeer2)) {
-                accountList.misBeer(2);
-                stockAcc2.setText(String.valueOf(accountList.get(2).getStock()));
-            } else if(eventButton.equals(misBeer3)) {
-                accountList.misBeer(3);
-                stockAcc3.setText(String.valueOf(accountList.get(3).getStock()));
-            } else if(eventButton.equals(misBeer4)) {
-                accountList.misBeer(4);
-                stockAcc4.setText(String.valueOf(accountList.get(4).getStock()));
-            } else if(eventButton.equals(misBeer5)) {
-                accountList.misBeer(5);
-                stockAcc5.setText(String.valueOf(accountList.get(5).getStock()));
+        for(int i = 0; i<6 ; i++){
+            if(event.getSource().equals(misBeerButtonList.get(i))){
+                accountList.misBeer(i);
+                accountStockList.get(i).setText(String.valueOf(accountList.get(i).getStock()));
             }
-            totalStock.setText(String.valueOf(accountList.getTotalStock()));
-            mainController.getMessageBoard().setText("Zieke feut die je bent");
-            mainController.write();
-            positiveBeer();
         }
+        totalStock.setText(String.valueOf(accountList.getTotalStock()));
+        mainController.getMessageBoard().setText("Zieke feut die je bent");
+        mainController.write();
+        positiveBeer();
+
     }
 
     /**
@@ -286,33 +246,16 @@ public class TurfViewController implements Initializable {
      */
     public void misKratButtonClicked(ActionEvent event){
         mainController.resetMessageBoard();
-        if(event.getSource() instanceof Button) {
-            Button eventButton = (Button) event.getSource();
-            mainController.getMessageBoard().setText("Zieke mega-feut die je bent");
-
-            if(eventButton.equals(misKrat0)){
-                accountList.misKrat(0);
-                stockAcc0.setText(String.valueOf(accountList.get(0).getStock()));
-            } else if(eventButton.equals(misKrat1)){
-                accountList.misKrat(1);
-                stockAcc1.setText(String.valueOf(accountList.get(1).getStock()));
-            } else if(eventButton.equals(misKrat2)){
-                accountList.misKrat(2);
-                stockAcc2.setText(String.valueOf(accountList.get(2).getStock()));
-            } else if(eventButton.equals(misKrat3)){
-                accountList.misKrat(3);
-                stockAcc3.setText(String.valueOf(accountList.get(3).getStock()));
-            } else if(eventButton.equals(misKrat4)){
-                accountList.misKrat(4);
-                stockAcc4.setText(String.valueOf(accountList.get(4).getStock()));
-            } else if(eventButton.equals(misKrat5)){
-                accountList.misKrat(5);
-                stockAcc5.setText(String.valueOf(accountList.get(5).getStock()));
+        for(int i = 0; i<6 ; i++){
+            if(event.getSource().equals(misKratButtonList.get(i))){
+                accountList.misKrat(i);
+                accountStockList.get(i).setText(String.valueOf(accountList.get(i).getStock()));
             }
-            totalStock.setText(String.valueOf(accountList.getTotalStock()));
-            mainController.write();
-            positiveBeer();
         }
+        mainController.getMessageBoard().setText("Zieke mega-feut die je bent");
+        totalStock.setText(String.valueOf(accountList.getTotalStock()));
+        mainController.write();
+        positiveBeer();
     }
 
     /**
@@ -321,69 +264,28 @@ public class TurfViewController implements Initializable {
      */
     public void kratKoopButtonClicked(ActionEvent event){
         mainController.resetMessageBoard();
-        if(event.getSource() instanceof Button) {
-            Button eventButton = (Button) event.getSource();
-            mainController.getMessageBoard().setText("Mooie lul die je bent");
-
-            if(eventButton.equals(kratKoop0)){
-                accountList.kratKoop(0);
-                stockAcc0.setText(String.valueOf(accountList.get(0).getStock()));
-            } else if(eventButton.equals(kratKoop1)){
-                accountList.kratKoop(1);
-                stockAcc1.setText(String.valueOf(accountList.get(1).getStock()));
-            } else if(eventButton.equals(kratKoop2)){
-                accountList.kratKoop(2);
-                stockAcc2.setText(String.valueOf(accountList.get(2).getStock()));
-            } else if(eventButton.equals(kratKoop3)){
-                accountList.kratKoop(3);
-                stockAcc3.setText(String.valueOf(accountList.get(3).getStock()));
-            } else if(eventButton.equals(kratKoop4)){
-                accountList.kratKoop(4);
-                stockAcc4.setText(String.valueOf(accountList.get(4).getStock()));
-            } else if(eventButton.equals(kratKoop5)){
-                accountList.kratKoop(5);
-                stockAcc5.setText(String.valueOf(accountList.get(5).getStock()));
+        for( int i = 0; i<6; i++){
+            if(event.getSource().equals(kratKoopButtonList.get(i))){
+                accountList.kratKoop(i);
+                accountStockList.get(i).setText(String.valueOf(accountList.get(i).getStock()));
             }
-            totalStock.setText(String.valueOf(accountList.getTotalStock()));
-            positiveBeer();
-            mainController.write();
         }
+        mainController.getMessageBoard().setText("Mooie lul die je bent");
+        totalStock.setText(String.valueOf(accountList.getTotalStock()));
+        positiveBeer();
+        mainController.write();
+
     }
 
     /**
      * A method setting the Background to white or red depending on stock value
      */
     public void positiveBeer(){
-        Color red = Color.RED;
-        Color white = Color.WHITE;
-        BackgroundFill redFill = new BackgroundFill(red, null, null);
-        BackgroundFill whiteFill = new BackgroundFill(white, null, null);
-        Background redBackground = new Background(redFill);
-        Background whiteBackground = new Background(whiteFill);
-        if(accountList.get(0).getStock()<0){
-            AccPane0.setBackground(redBackground);
-        } if(accountList.get(0).getStock()>= 0){
-            AccPane0.setBackground(whiteBackground);
-        } if(accountList.get(1).getStock()<0){
-            AccPane1.setBackground(redBackground);
-        } if(accountList.get(1).getStock()>= 0){
-            AccPane1.setBackground(whiteBackground);
-        } if(accountList.get(2).getStock()<0){
-            AccPane2.setBackground(redBackground);
-        } if(accountList.get(2).getStock()>= 0){
-            AccPane2.setBackground(whiteBackground);
-        } if(accountList.get(3).getStock()<0){
-            AccPane3.setBackground(redBackground);
-        } if(accountList.get(3).getStock()>= 0){
-            AccPane3.setBackground(whiteBackground);
-        } if(accountList.get(4).getStock()<0){
-            AccPane4.setBackground(redBackground);
-        } if(accountList.get(4).getStock()>= 0){
-            AccPane4.setBackground(whiteBackground);
-        } if(accountList.get(5).getStock()<0){
-            AccPane5.setBackground(redBackground);
-        } if(accountList.get(5).getStock()>= 0){
-            AccPane5.setBackground(whiteBackground);
+        Background redBackground = new Background(new BackgroundFill(Color.RED, null, null));
+        Background whiteBackground = new Background(new BackgroundFill(Color.WHITE, null, null));
+        for(int i = 0; i<6; i++){
+            if(accountList.get(i).getStock()>=0) accountPaneList.get(i).setBackground(whiteBackground);
+            if(accountList.get(i).getStock()<0) accountPaneList.get(i).setBackground(redBackground);
         }
     }
 }
